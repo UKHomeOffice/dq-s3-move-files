26c74d9dabaaad1df524c00ca82a7f8725eeae21
david-wirelab <info@wirelab.co.uk>
2018-11-19 14:34:55 +0000

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # dq-s3-move-files
+
+Bash script that moves files between key prefixes in S3.
+It created key prefixes based on the timestamp found in the file name.
+
+# Dependencies:
+ - aws cli
+ - s3cmd
+ - AWS creds and profile setup with *s3cmd*
+
+# S3CMD CLI modules to use:
+- s3cmd ls s3://bucket-name: to list the content
+- s3cmd mv s3://bucket-name/file.txt s3://bucket-name/subfolder/file.txt
+
+# Variables:
+
+||Variable Name||Value||Usage||
+|S3_BUCKET_NAME|bucket_name|Working S3 bucket|
+|SOURCE_PATH|src|Bucket source prefix|
+|DESTINATION_PATH|src|Bucket destination prefix|
+|REGEX|'PARSED_[0-9]{8}_[0-9]{4}_[0-9]{4}.zip'|Regular expression value|
+|FILE_LIMIT|100|Batch size|
---
+++ s4_parsed_file_move.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# This script will connect to an S3 bucket, list 25 files at the source location
+# A for loop will process one file at a tme and move it from the source to the destination object
+# It will then get the appropriate prefix to use based on the timestamp in the file name YYYY|MM|DD
+# Using REGEX it will start moving only the files that match the regex into the new prefixes based on the timestamp
+
+set -e
+
+S3_BUCKET_NAME=bucket_name
+SOURCE_PATH=src
+DESTINATION_PATH=src
+REGEX='PARSED_[0-9]{8}_[0-9]{4}_[0-9]{4}.zip'
+FILE_LIMIT=100
+
+# List items from S3
+function get_list_new {
+  array=$(s3cmd ls --limit $FILE_LIMIT s3://$S3_BUCKET_NAME/$SOURCE_PATH/ | awk '{$1=$2=$3=""; print $0}' | sed 's_s3://s3-dq-test/yel-1385-src/__' | sed 's/^[ \t]*//')
+  echo $array
+}
+
+# Move files to the new locations based on the file name obtained from the array
+function move_files {
+  declare -a array=$(get_list_new)
+  for file in ${array[@]}; do
+        echo "File found '${file}'"
+        YEAR=$(echo "${file:7:4}")
+        MONTH=$(echo "${file:11:2}")
+        DAY=$(echo "${file:13:2}")
+        s3cmd mv --no-progress --rexclude ".*"  --rinclude $REGEX s3://$S3_BUCKET_NAME/$SOURCE_PATH/${file} s3://$S3_BUCKET_NAME/$DESTINATION_PATH/$YEAR/$MONTH/$DAY/${file} || continue
+        echo "Copied file to S3 '${file}'"
+  done
+}
+
+# Create Main function
+function main {
+  move_files
+}
+
+main
+
+exit
